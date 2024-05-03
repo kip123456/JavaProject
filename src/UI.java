599e2d04a40db0a21ce8153ff3fda37995a629b7
k1p123456 <1003188723@qq.com>
2024-05-03 16:38:58 +0800

    add image transform function

--- src/UI.java
+++ src/UI.java
@@ -76,9 +76,13 @@ class MyJPanel extends JPanel {
      * @return
      */
     void paintChannels(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(7));
         for (int i = 0; i < Global.CHANNEL_COUNT; i++) {
-            g.setColor(Color.GRAY);
-            g.fillRect(lines[i].p1.x, lines[i].p1.y, lines[i+1].p1.x-lines[i].p1.x, lines[i].p2.y-lines[i].p1.y);
+            g2d.setColor(Color.GREEN);
+            g2d.fillRect(lines[i].p1.x, lines[i].p1.y, lines[i+1].p1.x-lines[i].p1.x, lines[i].p2.y-lines[i].p1.y);
+            g2d.setColor(Color.BLACK);
+            g2d.drawRect(lines[i].p1.x, lines[i].p1.y, lines[i+1].p1.x-lines[i].p1.x, lines[i].p2.y-lines[i].p1.y);
             System.out.println(lines[i].p1.x + " " + lines[i].p1.y + " " + (lines[i+1].p1.x - lines[i].p1.x) + " " + (lines[i].p2.y-lines[i].p1.y));
         }
     }
@@ -92,14 +96,50 @@ class MyJPanel extends JPanel {
             thing.repaint(g2d_buffered);
         }
         BufferedImage perspectiveChessBoard = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        for (int y = 0; y < image.getHeight(); y++) {
-            for (int x = 0; x < image.getWidth(); x++) {
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
                 //to do：区域判断，只有600*600的区域进行变换
                 //to do 2: 式子推导
-                int rgb = image.getRGB(x, y);
-                double f = 800.0 / (800 + y);
-                int xv = (int) (x * f);
-                int yv = (int) (y * f);
+
+                int x = i;
+                int y = j;
+
+                // 区域判断
+                if(x <= 295 || y <= 40) {
+                    continue;
+                }
+
+                /**
+                 * 坐标变换，目前打算是讲一个矩形映射成一个梯形
+                 */
+                y -= 44;
+                x -= 300;
+                int px = 150;
+                int py = -50;
+
+                // 用于改变上边的位置，ny越大，上边越低
+                int ny = 200;
+
+                // 映射y,不同的映射函数影响图形的长宽比例和速度
+                double tmp = 1.0*y/660;
+
+                // 二次函数，这个效果看起来长宽比例合适，但是会出现白边---有些地方画不到的情况
+                // y = ny + (int)(1.0*y/660*1.0*y/660*(660-ny));
+
+                // 线性函数
+                // y = ny + (int)(1.0*y/660*(660-ny));
+                y = ny + (int)(Math.pow(tmp, 1.4)*(660-ny));
+
+
+                x = x  + (int)((660.0 -y)/(660.0 - py)*(px-x));
+                int rgb = image.getRGB(i, j);
+                // double f = 800.0 / (800 + y);
+                // int xv = (int) (x * f);
+                // int yv = (int) (y * f);
+                int xv = x;
+                int yv = y;
+                xv += 300;
+                yv += 44;
                 if (xv >= 0 && xv < perspectiveChessBoard.getWidth() && yv >= 0 && yv < perspectiveChessBoard.getHeight()) {
                     perspectiveChessBoard.setRGB(xv, yv, rgb);
                 }
@@ -115,8 +155,8 @@ class MyJPanel extends JPanel {
         setBackground(Color.WHITE);
         for(int i = 0; i <= Global.CHANNEL_COUNT; i++) {
             lines[i] = new Line(
-                new Point(i*Global.CHANNEL_WIDTH, 0),
-                new Point(i*Global.CHANNEL_WIDTH, Global.CHANNEL_HEIGHT)
+                new Point(300+i*100, 44),
+                new Point(300+i*100, Global.CHANNEL_HEIGHT*4+100)
             );
         }
     }
