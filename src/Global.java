4823e89327c0197f2d2b7e320b21c797aacab705
k1p123456 <1003188723@qq.com>
2024-05-03 18:43:11 +0800

    modify player speed

--- src/Global.java
+++ src/Global.java
@@ -6,6 +6,6 @@ public class Global {
     public static final int WINDOW_WIDTH = 800;
     public static final int CHANNEL_PADDING=100;
     public static final int GENERATE_PADDING=12;
-    public static final int TICKS_PER_PLAYER_MOVE = 4;
+    public static final int TICKS_PER_PLAYER_MOVE = 8;
     public static final int MAX_PLAYER_JUMP = 10;
 }
