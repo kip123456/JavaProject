fb84ad0ca0067788ac834c7674c16ca07ff73bca
wssstc <2783245811@qq.com>
2024-05-25 15:35:08 +0800

    定义Saver接口，完善肉鸽内核数据处理

--- src/com/entity/Player.java
+++ src/com/entity/Player.java
@@ -1,6 +1,7 @@
 package com.entity;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
@@ -8,6 +9,7 @@ import javax.xml.crypto.Data;
 
 import com.Controller;
 import com.SEController;
+import com.SaveData;
 import com.data.DataManager;
 import com.data.Global;
 
@@ -20,11 +22,13 @@ public class Player {
     public int posx, posy, posz;
     public int lrSteps;
     public int udSteps;
+    public double rate[] = new double[10];
     public int inhurt;
     public int skill_loader;//技能条，10000为一次技能
-    public int skill_damage_rate;//技能伤害比率，初始为10倍攻击
+    public int skill_damage_rate;//技能伤害比率，初始为50倍攻击
     public int skill_load_speed;//技能恢复速度
     public int skill_store;//能存储多少技能蓄力
+    public boolean skill_hel_damage = false;
     public BufferedImage[] imgs;
     public Player() {
         health = 300;
@@ -37,15 +41,75 @@ public class Player {
         posz = 0;
         skill_store = 10000;
         imgs = DataManager.player_imgs;
-        skill_damage_rate = 10;
+        skill_damage_rate = 50;
         skill_loader = 0;
         skill_load_speed = Global.skill_load_speed;
     }
+    public Player(SaveData s) {
+        this();
+        money = s.coin;
+        for(int i=0;i<=3;++i)
+        {
+            if(s.sp[i])
+            health+=50*Math.pow(2, i);
+        }
+        for(int i=7;i<=10;++i)
+        {
+            if(s.sp[i])
+            attack+=(i-6)*2;
+        }
+        for(int i=14;i<=17;++i)
+        {
+            if(s.sp[i])
+            defense+=(i-13)*2;
+        }
+        for(int i=21;i<=24;++i)
+        {
+            if(s.sp[i])
+            magicDefense+=(i-20)*10;
+        }
+        for(int i=4;i<=6;++i)
+        if(s.sp[i])
+        rate[0]+=0.1;
+        for(int i=1;i<=3;++i)
+        {
+            for(int j=0;j<=2;++j)
+            {
+                if(s.sp[i*7+j+4])
+                rate[i]+=0.05;
+            }
+        }
+        if(s.sp[28])
+        skill_damage_rate+=10;
+        if(s.sp[29])
+        skill_hel_damage = true;
+        if(s.sp[30])
+        skill_load_speed=60;
+        if(s.sp[31])
+        skill_store*=2;
+        if(s.sp[32])
+        rate[4]=0.1;
+        if(s.sp[33])
+        rate[5]=0.1;
+    }
+    static Random rd = new Random(System.currentTimeMillis());
     public void react(T2PMessage msg) {
         if(msg.hel < 0)
         {
             inhurt = 3;
         }
+        if(rate[0]>0)
+        msg.hel*=(1+rate[0]);
+        if(rate[1]>0&&rd.nextInt(0, 10)*1.0/10<rate[1])
+        msg.atk*=2;
+        if(rate[2]>0&&rd.nextInt(0, 10)*1.0/10<rate[2])
+        msg.def*=2;
+        if(rate[3]>0&&rd.nextInt(0, 10)*1.0/10<rate[3])
+        msg.mdef*=2;
+        if(rate[4]>0&&rd.nextInt(0, 10)*1.0/10<rate[4])
+        msg.money*=2;
+        if(rate[5]>0&&rd.nextInt(0, 10)*1.0/10<rate[5])
+        msg.exp*=2;
         health += msg.hel;
         attack += msg.atk;
         defense += msg.def;
